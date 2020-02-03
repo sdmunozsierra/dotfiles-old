ebf45f937b1b9da62369923a33ae4345066b7e3f
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2020-02-03 12:27:21 -0500

    updated ssh config

--- ArchScripts/PostInstallScripts/3_ssh_config.sh
+++ ArchScripts/PostInstallScripts/3_ssh_config.sh
@@ -1,16 +1,75 @@
 #!/bin/bash
+DIR=~/.ssh/
 
 # Create ssh key pair
-# Ask user if new key pair is needed
+
+## Create ssh via email and location
+create_ssh_key(){
+    echo "Creating SSH key pair"
+    printf "Enter your email:\n"
+    read EMAIL
+    printf "Enter key filename (Press Enter for Default):\n"
+    read FILENAME
+    if [[$FILENAME = "" ]]; then
+        echo "Using default id_rsa filename"
+    else
+        echo "SSH key $DIR$FILENAME copied to clipboard"
+    fi
+    ssh-keygen -t rsa -b 4096 -C $EMAIL -f $DIR$FILENAME
+}
+
+## Print available keys
+get_available_keys(){
+    echo "These are the available files:"
+    ls ~/.ssh/*
+    echo "Choose key to copy to clipboard:\n"
+    read FILENAME
+}
+
+## Copy key
+copy_key(){
+    xclip -sel clip < $DIR$FILENAME.pub
+    echo "SSH key copied to clipboard"
+    echo "Upload the SSH key in github and gitlab"
+}
+
+## Update /.ssh/.config
+update_config_file(){
+    echo "Host (eg. es.github.com/git.annex.leidos.com):\n"
+    read HOST
+    echo "Hostname (eg. github.com/gitlab.com):\n"
+    read HOSTNAME
+    touch $DIR\config
+    printf "Host $HOST\nHostName $HOSTNAME\nPreferredAuthentications publickey\nIdentityFile $DIR$FILENAME\n" >> $DIR\config
+}
+
+## Create key control
+
 printf "Do you want to generate new SSH key pair? y/n\n"
-read  CONTRL
+read CONTRL
 
 case $CONTRL in
     yes|y)
-        echo "Creating SSH key pair"
-        printf "Enter your email:\n"
-        read  EMAIL
-        ssh-keygen -t rsa -b 4096 -C $EMAIL
+        create_ssh_key
+        ;;
+    no|n)
+        get_available_keys
+        ;;
+    *) 
+        echo "Type yes/y or no/n"
+        exit
+        ;;
+esac
+
+copy_key
+
+## Update .ssh/.config
+printf "Update .config file? y/n\n"
+read CONTRL
+
+case $CONTRL in
+    yes|y)
+        update_config_file
         ;;
     no|n)
         ;;
@@ -19,6 +78,3 @@ case $CONTRL in
         exit
         ;;
 esac
-xclip -sel clip < ~/.ssh/id_rsa.pub
-echo "SSH key copied to clipboard"
-echo "Upload the SSH key in github and gitlab"
